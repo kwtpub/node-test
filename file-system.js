6e41dc1e8c9050836c2e0e5d3200a9db783dee3f
kwtpub <10.0.kwt@mail.ru>
2025-02-12 00:14:44 +0400

    initial

---
+++ file-system.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+const path = require('path')
+
+// console.log('START')
+
+// fs.mkdir(path.resolve(__dirname,'dir','dir3','dir2'),(err) => {
+
+//     if(err) {
+//        console.log(err)
+//       return;
+//     }
+//    console.log('Папка создана')
+// })
+
+// console.log('END')
+
+// fs.rmdir(path.resolve(__dirname, 'dir'), (err) => {
+//     if(err) {
+//         throw err;
+//     }
+// })
+
+fs.writeFile(path.resolve)
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv')
 dotenv.config()
 
-// const path = require('path')
-// const fs = require('fs')
+const path = require('path')
+const fs = require('fs')
 
 // fs.mkdir(path.resolve(__dirname,'dir','dir3','dir2'), (err) => {
 
@@ -13,12 +13,16 @@ dotenv.config()
 //    console.log('Папка создана')
 // })
 
-// //const fullparse = path.join( __dirname,'first','second')
+const fullparse = path.join( __dirname,'first','second')
 
-// //console.log(path.parse(fullparse))
+console.log(path.parse(fullparse))
 
 // console.log(process.pid)
 // console.log(process.env.PORT)
 // console.log(process.env.NODE_ENV)
 // console.log(process.argv)
 
+const siteUrl = 'http://localhost:8080/users?id=5123'
+
+const url = new URL(siteUrl)
+console.log(url)

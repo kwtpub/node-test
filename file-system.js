b05b53cd70e7abb6fd5072547a2372462184527e
kwtpub <10.0.kwt@mail.ru>
2025-02-16 17:59:40 +0400

    ww

--- file-system.js
+++ file-system.js
@@ -20,16 +20,50 @@ const path = require('path')
 //     }
 // })
 
-fs.writeFile(path.resolve(__dirname, 'text.txt'), '43 sdfsdf 7dfg 6 ываыв 4', (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log('файл записан')
-} )
+// fs.writeFile(path.resolve(__dirname, 'text.txt'), '43 sdfsdf 7dfg 6 ываыв 4', (err) => {
+//     if(err) {
+//         throw err;
 
-fs.appendFile(path.resolve(__dirname, 'text.txt'), 'ДОБАВИЛИ В КОНЕЦ', (err) => {
-    if(err) {
-        throw err;
-    }
-    console.log('Файл записан')
-})
+//     }
+//     console.log('файл записан')
+// } )
+
+// fs.appendFile(path.resolve(__dirname, 'text.txt'), 'ДОБАВИЛИ В КОНЕЦ', (err) => {
+//     if(err) {
+//         throw err;
+
+//     }
+//     fs.appendFile(path.resolve(__dirname, 'text.txt'), 'ДОБАВИЛИ В КОНЕЦ', (err) => {
+//         if(err) {
+//             throw err;
+    
+//         }
+//         console.log('Файл записан')
+//     })
+// })
+
+const writeFileAsync = async (path,data) => { 
+    return new Promise((resolve, rejects) => fs.writeFile(path, data, (err) => { 
+        if(err) {
+            return rejects(err.message)
+        } 
+        resolve()
+
+    }))
+}
+
+const appendFileAsync = async (path,data) => { 
+    return new Promise((resolve, rejects) => fs.appendFile(path, data, (err) => { 
+        if(err) {
+            return rejects(err.message)
+        } 
+        resolve()
+
+    }))
+}
+
+writeFileAsync(path.resolve(__dirname,  'text.txt'), 'data')
+    .then(() => appendFileAsync(path.resolve(__dirname, 'text.txt'), '123'))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'text.txt'),'456'))
+    .then(() => appendFileAsync(path.resolve(__dirname, 'text.txt'),'789'))
+    .catch(err => console.log(err))
